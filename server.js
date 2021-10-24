9af7dbcf877d6d344d043be78fee7f764a724049
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-10-24 21:35:52 +0530

    error changed

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {getCodeObject} = require("./createObjFromJson");
+const cors=require("cors");
 
 const app = express();
 const port = 8080;
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            
+  optionSuccessStatus:200,
+}
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
+app.use(cors(corsOptions))
 
 app.post('/', async (req, res)=>
 {
   const run_var= getCodeObject(req.body);
   await  run_var.run_file();
-
-  if(!run_var.error)
   res.status(201).json({"error":run_var.error,"output":run_var.output});
-
-  else
-  res.status(401).json({"error":run_var.error,"output":run_var.output});
-
 });
 
 app.listen(port, () => {
